fe816b93918cfd6d826c16cbadf4726ac89c29a6
shaifalygupta <36529633+shaifalygupta@users.noreply.github.com>
2018-02-16 23:40:15 +1100

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,22 @@ import (
 
 /**
 Main Router file to handle all the request based on URI .
-This Supports GET & POST Http Method
+This file used gorilla/mux package for parameter substitution in the URL .
+This Supports GET & POST Http Method .
+Sample Urls for Reference
+http://localhost:8081/EPG/csv
+http://localhost:8081/EPG/csv/USA
+http://localhost:8081/EPG/csv/USA/MTV
+
+POST : http://localhost:8081/EPG/csv
+{"ID": "120015","Name":" Test Series","Type":"Program","SubType":"Episode","Duration":1800,"SourceSystem":"P33","Country":"USA","Channel":"MTV"}
+
+http://localhost:8081/EPG/mock
+http://localhost:8081/EPG/mock/USA
+http://localhost:8081/EPG/mock/USA/MTV
+
+POST : http://localhost:8081/EPG/mock
+{"ID": "120015","Name":" Test Series","Type":"Program","SubType":"Episode","Duration":1800,"SourceSystem":"P33","Country":"USA","Channel":"MTV"}
  */
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
